0ac6fcc51880a6e9cf2fe42406dae6f5fc69e229
Julien Lancelot <julien.lancelot@sonarsource.com>
2016-07-01 16:56:25 +0200

    DO NOT PUSH TO MASTER : Active QA

--- travis.sh
+++ travis.sh
@@ -30,7 +30,7 @@ CI)
         -Pdeploy-sonarsource \
         -B -e -V
 
-  elif [[ "${TRAVIS_BRANCH}" == "branch-"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
+  elif [[ "${TRAVIS_BRANCH}" == "feature/jl/retry_to_not_remove_session_when_401"* ]] && [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     strongEcho 'Build and deploy'
 
     # get current version from pom
